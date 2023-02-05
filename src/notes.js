81ed1d1c9d7e394870b77bdac2c28a5c6c91f4db
Felipe <felipe@demola.net>
2023-02-05 15:04:12 -0300

    generating until the human hearing

--- src/notes.js
+++ src/notes.js
@@ -2,7 +2,7 @@ export default function noteFrequencyMap(middleAFreq) {
     const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
     const noteFrequencyMap = {}
     const C0semitonesDistance = -57;
-    for (let i = 0; i < 108; i++) {
+    for (let i = 0; i < 109 + 16; i++) {
         const note = notes[i % 12];
         const octave = Math.floor(i / 12);
         noteFrequencyMap[note + octave] = calculateNote(middleAFreq, C0semitonesDistance + i);
