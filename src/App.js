2fb3234509c6efa9dae22a56fc81066315f843f6
Felipe <felipe@demola.net>
2023-02-05 14:27:51 -0300

    displaying a table of harmonics

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
 import './App.css';
 import noteFrequencyMap from './notes';
+import {useState} from "react";
+
+function calculateHarmonic(noteFrequency, harmonicNumber) {
+    return noteFrequency * harmonicNumber;
+}
 
 function App() {
-
+    let [selectedNotes, setSelectedNotes] = useState([]);
 
     let notesMap = noteFrequencyMap(440);
+
     return (
         <>
-            <div>
+            <div style={{float: "left"}}>
                 <h1>Options</h1>
+                <select multiple={true}
+                        onChange={selectNotes()}
+                        style={{height: '90vh', width: '40px'}}>
+                    {Object.entries(notesMap).map(([note, freq]) => {
+                        return <option key={note}
+                                       value={note}>{note}</option>
+                    })
+                    }
+                </select>
             </div>
-            <select multiple={true} style={{height:'90vh', width: '40px'}}>
-                {Object.entries(notesMap).map(([note, freq]) => {
-                    return <option value={note}>{note}</option>
-                })
-                }
-
-            </select>
             <div>
-
+                <h1>Selected Notes</h1>
+                <table border={1}>
+                    <thead>
+                    <tr>
+                        <th>Note</th>
+                        <th>1st harm</th>
+                        <th>2nd harm</th>
+                        <th>3rd harm</th>
+                        <th>4th harm</th>
+                        <th>5th harm</th>
+                        <th>6th harm</th>
+                        <th>7th harm</th>
+                        <th>8th harm</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {selectedNotes.map((note) => {
+                            return <tr key={note}>
+                                <td>{note + '(' + notesMap[note] + ')'}</td>
+                                <td>{calculateHarmonic(notesMap[note],1)}</td>
+                                <td>{calculateHarmonic(notesMap[note],2)}</td>
+                                <td>{calculateHarmonic(notesMap[note],3)}</td>
+                                <td>{calculateHarmonic(notesMap[note],4)}</td>
+                                <td>{calculateHarmonic(notesMap[note],5)}</td>
+                                <td>{calculateHarmonic(notesMap[note],6)}</td>
+                                <td>{calculateHarmonic(notesMap[note],7)}</td>
+                                <td>{calculateHarmonic(notesMap[note],8)}</td>
+                            </tr>
+                        }
+                    )}
+                    </tbody>
+                </table>
             </div>
         </>
     );
+
+    function selectNotes() {
+        return (e) => {
+            let selectedOptions = e.target.selectedOptions;
+            let selectedNotes = [];
+            for (let i = 0; i < selectedOptions.length; i++) {
+                selectedNotes.push(selectedOptions[i].value);
+            }
+            setSelectedNotes(selectedNotes);
+        };
+    }
 }
 
 
