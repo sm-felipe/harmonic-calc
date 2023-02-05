7c8474a1b9434a802165b28036b2ad1af4624376
Felipe <felipe@demola.net>
2023-02-05 14:43:08 -0300

    formatting the collumns

--- src/App.js
+++ src/App.js
@@ -13,51 +13,53 @@ function App() {
 
     return (
         <>
-            <div style={{float: "left"}}>
-                <h1>Options</h1>
-                <select multiple={true}
-                        onChange={selectNotes()}
-                        style={{height: '90vh', width: '40px'}}>
-                    {Object.entries(notesMap).map(([note, freq]) => {
-                        return <option key={note}
-                                       value={note}>{note}</option>
-                    })
-                    }
-                </select>
-            </div>
-            <div>
-                <h1>Selected Notes</h1>
-                <table border={1}>
-                    <thead>
-                    <tr>
-                        <th>Note</th>
-                        <th>1st harm</th>
-                        <th>2nd harm</th>
-                        <th>3rd harm</th>
-                        <th>4th harm</th>
-                        <th>5th harm</th>
-                        <th>6th harm</th>
-                        <th>7th harm</th>
-                        <th>8th harm</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {selectedNotes.map((note) => {
-                            return <tr key={note}>
-                                <td>{note + '(' + notesMap[note] + ')'}</td>
-                                <td>{calculateHarmonic(notesMap[note],1)}</td>
-                                <td>{calculateHarmonic(notesMap[note],2)}</td>
-                                <td>{calculateHarmonic(notesMap[note],3)}</td>
-                                <td>{calculateHarmonic(notesMap[note],4)}</td>
-                                <td>{calculateHarmonic(notesMap[note],5)}</td>
-                                <td>{calculateHarmonic(notesMap[note],6)}</td>
-                                <td>{calculateHarmonic(notesMap[note],7)}</td>
-                                <td>{calculateHarmonic(notesMap[note],8)}</td>
-                            </tr>
+            <div className={'flex-container'}>
+                <div className={'flex-child'}>
+                    <h1>Options</h1>
+                    <select multiple={true}
+                            onChange={selectNotes()}
+                            style={{height: '90vh', width: '40px'}}>
+                        {Object.entries(notesMap).map(([note]) => {
+                            return <option key={note}
+                                           value={note}>{note}</option>
+                        })
                         }
-                    )}
-                    </tbody>
-                </table>
+                    </select>
+                </div>
+                <div className={'flex-child'}>
+                    <h1>Selected Notes</h1>
+                    <table border={1}>
+                        <thead>
+                        <tr>
+                            <th>Note</th>
+                            <th>1st harm</th>
+                            <th>2nd harm</th>
+                            <th>3rd harm</th>
+                            <th>4th harm</th>
+                            <th>5th harm</th>
+                            <th>6th harm</th>
+                            <th>7th harm</th>
+                            <th>8th harm</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {selectedNotes.map((note) => {
+                                return <tr key={note}>
+                                    <td>{note + '(' + notesMap[note] + ')'}</td>
+                                    <td>{calculateHarmonic(notesMap[note], 1)}</td>
+                                    <td>{calculateHarmonic(notesMap[note], 2)}</td>
+                                    <td>{calculateHarmonic(notesMap[note], 3)}</td>
+                                    <td>{calculateHarmonic(notesMap[note], 4)}</td>
+                                    <td>{calculateHarmonic(notesMap[note], 5)}</td>
+                                    <td>{calculateHarmonic(notesMap[note], 6)}</td>
+                                    <td>{calculateHarmonic(notesMap[note], 7)}</td>
+                                    <td>{calculateHarmonic(notesMap[note], 8)}</td>
+                                </tr>
+                            }
+                        )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     );
