cf4d7b8d0fd8c11eceb6c594bfc3135cc912f88a
Felipe <felipe@demola.net>
2023-02-25 13:48:20 -0300

    sample spectogram

--- src/components/Spectogram.jsx
+++ src/components/Spectogram.jsx
@@ -1,76 +1,38 @@
-import {Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {CartesianGrid, Cell, Scatter, ScatterChart, Tooltip, XAxis, YAxis} from "recharts";
 import React from "react";
 
-
 const data = [
-    {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
+    { x: 100, y: 200, z: 200 },
+    { x: 120, y: 100, z: 260 },
+    { x: 170, y: 300, z: 400 },
+    { x: 140, y: 250, z: 280 },
+    { x: 150, y: 400, z: 500 },
+    { x: 110, y: 280, z: 200 },
 ];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
 
 export function Spectogram({harmonicMatrix}) {
     return (
-        <div style={{height: '500px', width: '500px'}}>
-            <ResponsiveContainer width="100%" height="100%">
-                <BarChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 20,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <XAxis dataKey="name"/>
-                    <YAxis/>
-                    <Tooltip/>
-                    <Legend/>
-                    <Bar dataKey="pv" stackId="a" fill="#8884d8"/>
-                    <Bar dataKey="uv" stackId="a" fill="#82ca9d"/>
-                </BarChart>
-            </ResponsiveContainer>
-        </div>
+        <ScatterChart
+            width={400}
+            height={400}
+            margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+            }}
+        >
+            <CartesianGrid />
+            <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+            <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter name="A school" data={data} fill="#8884d8">
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+            </Scatter>
+        </ScatterChart>
     );
 }
